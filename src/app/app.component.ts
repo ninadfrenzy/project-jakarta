09484195d7c412771a5c17b51fbfc0179b003a21
Ninad Gajanan Manjaramkar <NManjaramkar@slb.com>
2022-12-03 13:28:42 +0530

    [add] firestore configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Item {
+  name: string
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-jakarta';
+  item$: Observable<Item[]>;
+  constructor(firestore: Firestore) {
+    const collectionReference = collection(firestore, 'sample');
+    this.item$ = collectionData(collectionReference) as Observable<Item[]>;
+    this.item$.subscribe({
+      next: (data) => {
+        console.log(data);
+        
+      }
+    })
+  }
 }
